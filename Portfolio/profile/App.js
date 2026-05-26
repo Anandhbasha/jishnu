3a79a3e8e7328fa33df33169c307285403bf32f7
Anandhbasha <anadhakm@gmail.com>
2026-05-26 09:04:46 +0530

    screens

--- Portfolio/profile/App.js
+++ Portfolio/profile/App.js
@@ -1,16 +1,20 @@
-import {  NavigationContainer } from "@react-navigation/native"
-import Login from "./Screens/Login"
-import Home from "./Screens/Home"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-const Stack = createNativeStackNavigator()
-const App = ()=>{
-  <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen  initialRouterName="Login"/>
-        <Stack.Screen name ="Login" components = {<Login/>} />
-        <Stack.Screen name ="Home" components = {<Home/>} />
+import Login from "./Screens/Login";
+import Home from "./Screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Home" component={Home}/>
       </Stack.Navigator>
-  </NavigationContainer>
-}
-export default App
+    </NavigationContainer>
+  );
+};
+
+export default App;
